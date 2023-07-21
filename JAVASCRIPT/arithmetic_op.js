f142612028540cc878470944299cf5636142bb40
nranjan01 <139685921+nranjan01@users.noreply.github.com>
2023-07-21 11:07:23 +0530

    Create arithmetic_op.js

---
+++ JAVASCRIPT/arithmetic_op.js
@@ -0,0 +1,8 @@
+function addNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+const number1 = 5;
+const number2 = 10;
+const sum = addNumbers(number1, number2);
+console.log("The sum of", number1, "and", number2, "is:", sum);
